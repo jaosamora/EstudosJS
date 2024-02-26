755fcc36f7caddde0e6b54edae53f4b6513159e5
jaosamora <160861178+jaosamora@users.noreply.github.com>
2024-02-26 10:04:09 -0300

    Adicionando nova pasta

---
+++ aula16/index.js
@@ -0,0 +1,34 @@
+//               0       1       2 
+const alunos = ['Luiz', 'Maria','João'];
+
+
+
+// delete alunos[1];
+// console.log(alunos);
+
+// const removido = alunos.shift();
+// console.log(removido);
+// console.log(alunos);
+
+// const removido = alunos.pop(); //remove indice do fim
+
+// alunos.unshift('Luiza') // Adiciona no começo
+
+
+// alunos.push('Luiza'); // Adiciona no fim
+// alunos.push('Fábio'); //Adiciona no fim
+
+
+
+// alunos[alunos.length] = 'Luiza' //Adiciona no fim
+// alunos[alunos.length] = 'Fábio'
+// alunos[alunos.length] = 'Luana'
+
+
+// alunos[0] = 'Eduardo'; // Altera
+// alunos[3] = 'Luiza';// adicionando
+
+
+// console.log(alunos);
+// console.log(alunos[0]);
+// console.log(alunos[2]);
