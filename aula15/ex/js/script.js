97a7536f761ae6c188a4222d59d18667dc932cf6
jaosamora <160861178+jaosamora@users.noreply.github.com>
2024-02-26 09:55:51 -0300

    Adicionando pasta ao repositório

---
+++ aula15/ex/js/script.js
@@ -0,0 +1,14 @@
+const numero = Number(prompt('Digite um nume'));
+ const numeroTitulo = document.getElementById('numero-titulo');
+ const texto = document.getElementById('texto');
+
+
+ numeroTitulo.innerHTML = numero;
+
+ texto.innerHTML = ''
+ texto.innerHTML += `<p>Raiz quadrada: ${numero ** 0.5}</p>`;
+ texto.innerHTML += `<P>É NaN ${Number.isNaN(numero)}.</P>`
+ texto.innerHTML += `<p>${numero} é inteiro: ${Number.isInteger(numero)}</p>`;
+ texto.innerHTML += `<p>Arredondado para baixo: ${Math.floor(numero)}</p>`;
+ texto.innerHTML += `<p>Arredondado para cima: ${Math.ceil(numero)}</p>`;
+ texto.innerHTML += `<p>Com duas casas decimais: ${numero.toFixed(2)}</p>`;
