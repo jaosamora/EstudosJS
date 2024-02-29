c044e1e36b702add95fd2f70e340b6397602edb5
jaosamora <160861178+jaosamora@users.noreply.github.com>
2024-02-29 09:52:44 -0300

    Adicionando aula 19

---
+++ aula19/index.js
@@ -0,0 +1,6 @@
+/*
+Primitivos (imutáveis) - string, number, boolean, undefined,
+null (bigint. symbol) - Valores copiados
+
+referência (mutável) - array, object, function - Passados por referência 
+*/
