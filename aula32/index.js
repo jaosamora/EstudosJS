7196647fed4d21cdc718b603644c178bfe98b9bd
jaosamora <160861178+jaosamora@users.noreply.github.com>
2024-04-03 10:12:21 -0300

    Adicionando aula 32

---
+++ aula32/index.js
@@ -0,0 +1,18 @@
+/** 
+const pessoa = {
+    nome: 'João',
+    idade: 15
+  };
+  
+  const nome = pessoa.nome;
+  const idade = pessoa.idade;
+  */
+
+  const pessoa = {
+    nome: 'João',
+    idade: 15
+  };
+  
+  const { nome, idade } = pessoa;
+  
+  console.log();
