b69213a389f591b57211a5109153dd33ba5a3081
jaosamora <160861178+jaosamora@users.noreply.github.com>
2024-04-06 15:15:15 -0300

    Adicionando aula 37

---
+++ aula37/index.js
@@ -0,0 +1,15 @@
+// Break
+for (let i = 0; i < 5; i++) {
+    if (i === 3) {
+        break;
+    }
+    console.log(i);
+}
+
+//continue
+for (let i = 0; i < 5; i++) {
+    if (i === 2) {
+        continue;
+    }
+    console.log(i);
+}
