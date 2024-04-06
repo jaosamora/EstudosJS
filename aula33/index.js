016ca2d5969339f5adf904c6ac5f74785451c87b
jaosamora <160861178+jaosamora@users.noreply.github.com>
2024-04-06 15:05:07 -0300

    Adicionando aula 33

---
+++ aula33/index.js
@@ -0,0 +1,11 @@
+// console.log('Linha 0');
+// console.log('Linha 1');
+// console.log('Linha 2');
+// console.log('Linha 3');
+// console.log('Linha 4');
+// console.log('Linha 5');
+
+// i - index
+for (let i = 0; i <= 5; i++) {
+ console.log(`linha ${i}`)
+}
