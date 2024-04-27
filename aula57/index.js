f4e15a36c7938dff433089bb2d57449751982257
jaosamora <160861178+jaosamora@users.noreply.github.com>
2024-04-27 20:07:14 -0300

    Adicionando aula 57

---
+++ aula57/index.js
@@ -0,0 +1,12 @@
+const nomes = ['Maria', 'Luiz', 'Edurardo', 'Gabriel', 'Júlia'];
+
+//Unshift
+nomes.splice(2, 0, 'João', 'Paulo')
+console.log(nomes);
+
+// nomes.splice(índice atual, delete, elem2, elem3);
+// pop
+// const removidos = nomes.splice(-1, 1);
+
+// shift
+// const removidos = nomes.splice(0,1);
